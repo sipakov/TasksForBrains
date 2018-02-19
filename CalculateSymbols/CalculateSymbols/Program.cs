267c866026885ec35ff78c5aadd02f37b35850c7
Stanislav <sipakov1987@gmail.com>
2018-02-19 08:11:04 +0300

    Add files via upload

---
+++ CalculateSymbols/CalculateSymbols/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CalculateSymbols
+{
+    class Program
+    {
+      
+        static Dictionary<char, int> getCount(string s)
+        {
+            Dictionary<char, int> dict = new Dictionary<char, int>();
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (!dict.ContainsKey(s[i]))
+                {
+                    dict.Add(s[i], 1);
+                }
+                else
+                {
+                    dict[s[i]]++;
+                }
+
+            }
+            return dict;
+        }
+        static void Main(string[] args)
+        {
+            string s = "abcddcaaa";
+           var result = getCount(s);
+            foreach (var item in result)
+            {
+                Console.WriteLine($"{item.Key} {item.Value}");
+                
+            }
+            Console.ReadLine();
+        }
+    }
+}
